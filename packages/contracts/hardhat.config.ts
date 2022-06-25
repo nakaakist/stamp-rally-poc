ac18d592703a12126bc55edc68b5a9fb7ea4c7cc
Nariyuki Saito <toshincompos@gmail.com>
2022-06-25 11:32:10 +0900

    Add mumbai to contracts

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -29,6 +29,10 @@ const config: HardhatUserConfig = {
       url: process.env.GOERLI_URL || '',
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    mumbai: {
+      url: process.env.MUMBAI_URL || '',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
